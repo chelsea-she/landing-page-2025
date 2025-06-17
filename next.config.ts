adba2acdf017b2e5a1fb52782874f6b8bbfa19b5
chelsea-she <11chelseashe@gmail.com>
2025-06-17 01:12:00 -0400

    change next.config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
-    unoptimized: true, // prevents errors with next/image
+    unoptimized: true,
   },
 };
-
 module.exports = nextConfig;
