e1396e7bc929f2b51e30830aecc2f564d77f6d6f
chelsea-she <11chelseashe@gmail.com>
2025-06-17 01:31:08 -0400

    updated repo name

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/github.com/chelsea-she/landing-page-2025.git",
+  assetPrefix: "/github.com/chelsea-she/landing-page-2025.git",
   images: {
     unoptimized: true,
   },
